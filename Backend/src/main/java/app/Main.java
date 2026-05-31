a7147fd07b23134b148c78905b6bfea30950b3e2
Dimaroca <dimateoroca@gmail.com>
2026-05-31 08:43:53 -0600

    Estructura corregida

--- Backend/src/main/java/app/Main.java
+++ Backend/src/main/java/app/Main.java
@@ -1,24 +1,21 @@
 package app;
+import database.Neo4jManager;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // URI de conexión al servidor Neo4j (protocolo bolt)
         String uri = "neo4j://127.0.0.1:7687";
 
-        // Establecer las credenciales de autenticación
         String user = "neo4j";
         String password = "12345678";
 
-        // Se usa try-with-resources para cerrar la conexión automáticamente
-        try (Neo4jManager neo4j =
-                     new Neo4jManager(uri, user, password)) {
+        try (Neo4jManager neo4j = new Neo4jManager(uri, user, password)) {
 
-            // Verificar que la conexión al servidor sea exitosa
+           
             if (neo4j.testConnection()) {
 
-                // Cargar el dataset en la base de datos
                 neo4j.loadDataset();
 
                 System.out.println("OK");
